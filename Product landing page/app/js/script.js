f566785f6741304001796baa0d9e15729715ce63
nunoserodio <34492208+nunoserodio@users.noreply.github.com>
2021-06-19 23:26:10 +0100

    converted to css and worked on burger menu

--- Product landing page/app/js/script.js
+++ Product landing page/app/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(hamburger.classList.contains('open')){
+        hamburger.classList.remove('open');
+    }
+    else {
+        hamburger.classList.add('open');
+    }
+});
