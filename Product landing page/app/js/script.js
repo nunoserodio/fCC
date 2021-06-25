0d053767e2359e7f891c6e501a460643965e40eb
nunoserodio <34492208+nunoserodio@users.noreply.github.com>
2021-06-25 02:08:47 +0100

    tweaked animations and mobile burger menu

--- Product landing page/app/js/script.js
+++ Product landing page/app/js/script.js
@@ -1,12 +1,26 @@
 const hamburger = document.querySelector('#hamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeObjs = document.querySelectorAll('.fade');
 
 hamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
-    if(hamburger.classList.contains('open')){
-        hamburger.classList.remove('open');
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('no-scroll');
+        header.classList.remove('open');
+        fadeObjs.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
-    else {
-        hamburger.classList.add('open');
+    else { // Open Hamgurger Menu
+        body.classList.add('no-scroll');
+        header.classList.add('open');
+        fadeObjs.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
 });
